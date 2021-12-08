22d37b5897e301b64be782a44125a7deec72f19d
Idowu Julius <idowujulius92@gmail.com>
2021-12-08 20:40:03 +0100

    add auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.usersRepository.createUser(authCredentialsDto);
+  }
+}
